4db5e0d164bc8afdd5e73dba43de913fafa88e98
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2024-04-22 11:57:41 -0300

    avance

---
+++ src/entities/province.js
@@ -0,0 +1,20 @@
+import config from './src/configs/db-configs.js'
+import pkg from 'pg' 
+const { Client }  = pkg;
+
+export default Province class
+getallAsync() {
+
+}
+getByIdAsync() {
+
+}
+createAsync() {
+
+}
+updateAsync() {
+
+}
+deleteBy() {
+
+}
